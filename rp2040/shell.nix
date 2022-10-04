a3b29e47c76e9313be3f1f317c80edce2e495ef7
Artemis Tosini <me@artem.ist>
2022-10-04 19:24:47 +0000

    Update pico SDK

--- rp2040/shell.nix
+++ rp2040/shell.nix
@@ -4,8 +4,8 @@ let
     fetchSubmodules = true;
     owner = "raspberrypi";
     repo = "pico-sdk";
-    rev = "1.3.1";
-    sha256 = "sha256-Xf8QSQIe7XCMunljioC5/MhXcuTmyN7J6pPQFM3ico8=";
+    rev = "1.4.0";
+    sha256 = "sha256-i3IAaNcd0EfKNvU104a776O1poDAChlx1m+nP8iFn8E=";
   };
 in
 mkShell rec {
@@ -21,8 +21,8 @@ mkShell rec {
     gcc
     libusb
     openocd
-    picotool
     gcc-arm-embedded
+    picotool
   ];
 
   PICO_SDK_PATH = sdk_src;
